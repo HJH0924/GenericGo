25b6c8ccf37f6ec5b9887bf8e56259206f6f5644
韩嘉浩 <1165411520@qq.com>
2024-09-24 14:36:12 +0800

    feat: 随机数字、大小写字母、特殊字符的生成

--- randx/rand_code.go
+++ randx/rand_code.go
@@ -8,3 +8,127 @@
 **/
 
 package randx
+
+import (
+	"math/rand"
+
+	"github.com/HJH0924/GenericGo/errs"
+	"github.com/HJH0924/GenericGo/tuple"
+)
+
+type Type int
+
+const (
+	TypeDigit     Type = 1 << iota // 1 - 0001
+	TypeLowerCase                  // 2 - 0010
+	TypeUpperCase                  // 4 - 0100
+	TypeSpecial                    // 8 - 1000
+
+	TypeAllMixed = TypeDigit | TypeLowerCase | TypeUpperCase | TypeSpecial // 15 - 1111
+)
+
+const (
+	CharsetDigit     = `0123456789`
+	CharsetLowerCase = `abcdefghijklmnopqrstuvwxyz`
+	CharsetUpperCase = `ABCDEFGHIJKLMNOPQRSTUVWXYZ`
+	CharsetSpecial   = ` ~!@#$%^&*()_+-=[]{};'\\:\"|,./<>?`
+)
+
+var (
+	typeCharsetPairs = []tuple.Pair[Type, string]{
+		tuple.NewPair(TypeDigit, CharsetDigit),
+		tuple.NewPair(TypeLowerCase, CharsetLowerCase),
+		tuple.NewPair(TypeUpperCase, CharsetUpperCase),
+		tuple.NewPair(TypeSpecial, CharsetSpecial),
+	}
+)
+
+// RandStrByType 根据传入的长度和类型生成随机字符串。
+// 请保证输入的 length >= 0，否则会返回 NewErrLengthLessThanZero
+// 请保证输入的 typ 的取值范围在 (0, TypeAllMixed] 内，否则会返回 NewErrTypeNotSupported
+func RandStrByType(length int, typ Type) (string, error) {
+	if length < 0 {
+		return "", NewErrLengthLessThanZero()
+	}
+	if length == 0 {
+		return "", nil
+	}
+	if typ <= 0 || typ > TypeAllMixed {
+		return "", NewErrTypeNotSupported()
+	}
+
+	charset := ""
+	for _, pair := range typeCharsetPairs {
+		if (typ & pair.Key) == pair.Key {
+			charset += pair.Val
+		}
+	}
+
+	if charset == "" {
+		return "", NewErrEmptyCharset()
+	}
+
+	return randStr(length, charset), nil
+}
+
+// RandStrByCharset 根据传入的长度和字符集生成随机字符串。
+// 请保证输入的 length >= 0，否则会返回 NewErrLengthLessThanZero。
+// 请保证输入的字符集不为空字符串，否则会返回 NewErrEmptyCharset。
+// 字符集内部字符可以无序或重复。
+func RandStrByCharset(length int, charset string) (string, error) {
+	if length < 0 {
+		return "", NewErrLengthLessThanZero()
+	}
+	if length == 0 {
+		return "", nil
+	}
+	if charset == "" {
+		return "", NewErrEmptyCharset()
+	}
+
+	return randStr(length, charset), nil
+}
+
+func randStr(length int, charset string) string {
+	idxBits := 0
+	charsetSize := len(charset)
+	for charsetSize > (1<<idxBits)-1 {
+		idxBits++
+	}
+
+	idxMask := (1 << idxBits) - 1
+	remain := 63 / idxBits
+	cache := rand.Int63()
+	res := make([]byte, length)
+
+	for i := 0; i < length; {
+		if remain == 0 {
+			cache, remain = rand.Int63(), 63/idxBits
+		}
+
+		if randIdx := int(cache & int64(idxMask)); randIdx < charsetSize {
+			res[i] = charset[randIdx]
+			i++
+		}
+
+		cache >>= idxBits
+		remain--
+	}
+
+	return string(res)
+}
+
+// NewErrTypeNotSupported 创建一个表示不支持的类型的新错误。
+func NewErrTypeNotSupported() error {
+	return errs.WrapError("Unsupported type")
+}
+
+// NewErrLengthLessThanZero 创建一个表示长度必须大于等于0的新错误。
+func NewErrLengthLessThanZero() error {
+	return errs.WrapError("length must be greater than or equal to zero")
+}
+
+// NewErrEmptyCharset 创建一个表示字符集不能为空的新错误。
+func NewErrEmptyCharset() error {
+	return errs.WrapError("charset cannot be empty")
+}
--- randx/rand_code_test.go
+++ randx/rand_code_test.go
@@ -8,3 +8,271 @@
 **/
 
 package randx
+
+import (
+	"regexp"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRandStrByType(t *testing.T) {
+	tests := []struct {
+		name      string
+		length    int
+		typ       Type
+		wantMatch string
+		wantErr   error
+	}{
+		{
+			name:      "数字验证码",
+			length:    100,
+			typ:       TypeDigit, // 1
+			wantMatch: `^[0-9]+$`,
+			wantErr:   nil,
+		},
+		{
+			name:      "小写字母验证码",
+			length:    100,
+			typ:       TypeLowerCase, // 2
+			wantMatch: `^[a-z]+$`,
+			wantErr:   nil,
+		},
+		{
+			name:      "数字+小写字母验证码",
+			length:    100,
+			typ:       TypeDigit | TypeLowerCase, // 3
+			wantMatch: `^[a-z0-9]+$`,
+			wantErr:   nil,
+		},
+		{
+			name:      "大写字母验证码",
+			length:    100,
+			typ:       TypeUpperCase, // 4
+			wantMatch: `^[A-Z]+$`,
+			wantErr:   nil,
+		},
+		{
+			name:      "数字+大写字母验证码",
+			length:    100,
+			typ:       TypeUpperCase, // 5
+			wantMatch: `^[A-Z0-9]+$`,
+			wantErr:   nil,
+		},
+		{
+			name:      "大小写字母验证码",
+			length:    100,
+			typ:       TypeLowerCase | TypeUpperCase, // 6
+			wantMatch: `^[a-zA-Z]+$`,
+			wantErr:   nil,
+		},
+		{
+			name:      "数字+大小写字母验证码",
+			length:    100,
+			typ:       TypeDigit | TypeLowerCase | TypeUpperCase, // 7
+			wantMatch: `^[0-9a-zA-Z]+$`,
+			wantErr:   nil,
+		},
+		{
+			name:      "特殊字符验证码",
+			length:    100,
+			typ:       TypeSpecial, // 8
+			wantMatch: `^[^0-9a-zA-Z]+$`,
+			wantErr:   nil,
+		},
+		{
+			name:      "数字+特殊字符+大小写字母验证码",
+			length:    100,
+			typ:       TypeAllMixed, // 15
+			wantMatch: `^[\S\s]+$`,
+			wantErr:   nil,
+		},
+		{
+			name:      "未定义类型(超过范围)",
+			length:    100,
+			typ:       TypeAllMixed + 1,
+			wantMatch: "",
+			wantErr:   NewErrTypeNotSupported(),
+		},
+		{
+			name:      "未定义类型(0)",
+			length:    100,
+			typ:       0,
+			wantMatch: "",
+			wantErr:   NewErrTypeNotSupported(),
+		},
+		{
+			name:      "长度小于0",
+			length:    -1,
+			typ:       0,
+			wantMatch: "",
+			wantErr:   NewErrLengthLessThanZero(),
+		},
+		{
+			name:      "长度等于0",
+			length:    0,
+			typ:       TypeAllMixed,
+			wantMatch: "",
+			wantErr:   nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			str, err := RandStrByType(tt.length, tt.typ)
+			if err != nil {
+				assert.Equal(t, tt.wantErr, err)
+				assert.Equal(t, "", str)
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, tt.length, len(str))
+				matched, err := regexp.MatchString(tt.wantMatch, str)
+				assert.NoError(t, err)
+				assert.True(t, matched)
+			}
+		})
+	}
+}
+
+func TestRandStrByCharset(t *testing.T) {
+	tests := []struct {
+		name    string
+		length  int
+		charset string
+		wantErr error
+	}{
+		{
+			name:    "长度小于0",
+			length:  -1,
+			charset: CharsetDigit,
+			wantErr: NewErrLengthLessThanZero(),
+		},
+		{
+			name:    "长度等于0",
+			length:  0,
+			charset: CharsetDigit,
+			wantErr: nil,
+		},
+		{
+			name:    "字符集为空",
+			length:  100,
+			charset: "",
+			wantErr: NewErrEmptyCharset(),
+		},
+		{
+			name:    "数字验证码",
+			length:  100,
+			charset: CharsetDigit, // 1
+			wantErr: nil,
+		},
+		{
+			name:    "小写字母验证码",
+			length:  100,
+			charset: CharsetLowerCase, // 2
+			wantErr: nil,
+		},
+		{
+			name:    "数字+小写字母验证码",
+			length:  100,
+			charset: CharsetDigit + CharsetLowerCase, // 3
+			wantErr: nil,
+		},
+		{
+			name:    "大写字母验证码",
+			length:  100,
+			charset: CharsetUpperCase, // 4
+			wantErr: nil,
+		},
+		{
+			name:    "数字+大写字母验证码",
+			length:  100,
+			charset: CharsetDigit + CharsetUpperCase, // 5
+			wantErr: nil,
+		},
+		{
+			name:    "大小写字母验证码",
+			length:  100,
+			charset: CharsetLowerCase + CharsetUpperCase, // 6
+			wantErr: nil,
+		},
+		{
+			name:    "数字+大小写字母验证码",
+			length:  100,
+			charset: CharsetDigit + CharsetLowerCase + CharsetUpperCase, // 7
+			wantErr: nil,
+		},
+		{
+			name:    "特殊字符验证码",
+			length:  100,
+			charset: CharsetSpecial, // 8
+			wantErr: nil,
+		},
+		{
+			name:    "数字+特殊字符验证码",
+			length:  100,
+			charset: CharsetDigit + CharsetSpecial, // 9
+			wantErr: nil,
+		},
+		{
+			name:    "小写字母+特殊字符验证码",
+			length:  100,
+			charset: CharsetLowerCase + CharsetSpecial, // 10
+			wantErr: nil,
+		},
+		{
+			name:    "数字+小写字母+特殊字符验证码",
+			length:  100,
+			charset: CharsetDigit + CharsetLowerCase + CharsetSpecial, // 11
+			wantErr: nil,
+		},
+		{
+			name:    "大写字母+特殊字符验证码",
+			length:  100,
+			charset: CharsetUpperCase + CharsetSpecial, // 12
+			wantErr: nil,
+		},
+		{
+			name:    "数字+大写字母+特殊字符验证码",
+			length:  100,
+			charset: CharsetDigit + CharsetUpperCase + CharsetSpecial, // 13
+			wantErr: nil,
+		},
+		{
+			name:    "大小写字母+特殊字符验证码",
+			length:  100,
+			charset: CharsetLowerCase + CharsetUpperCase + CharsetSpecial, // 14
+			wantErr: nil,
+		},
+		{
+			name:    "数字+特殊字符+大小写字母验证码",
+			length:  100,
+			charset: CharsetDigit + CharsetSpecial + CharsetLowerCase + CharsetUpperCase, // 15
+			wantErr: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			str, err := RandStrByCharset(tt.length, tt.charset)
+			if err != nil {
+				assert.Equal(t, tt.wantErr, err)
+				assert.Equal(t, "", str)
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, tt.length, len(str))
+				assert.NoError(t, err)
+				assert.True(t, matchFunc(str, tt.charset))
+			}
+		})
+	}
+}
+
+func matchFunc(randStr string, charset string) bool {
+	for _, char := range randStr {
+		if !strings.ContainsRune(charset, char) {
+			return false
+		}
+	}
+	return true
+}
