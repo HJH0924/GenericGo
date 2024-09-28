64ab73069730d0a3b49b53b43543d80f63e4c0cd
韩嘉浩 <1165411520@qq.com>
2024-09-28 17:38:22 +0800

    style: update

--- list/linked_list_test.go
+++ list/linked_list_test.go
@@ -14,6 +14,7 @@ import (
 	"testing"
 
 	"errors"
+
 	"github.com/HJH0924/GenericGo/errs"
 	"github.com/stretchr/testify/assert"
 )
---
+++ pool/task_pool_test.go
@@ -0,0 +1,10 @@
+// Package pool
+/**
+* @Project : GenericGo
+* @File    : task_pool_test.go
+* @IDE     : GoLand
+* @Author  : Tvux
+* @Date    : 2024/9/28 17:12
+**/
+
+package pool
