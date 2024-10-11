15591f9fe4bff4aeb5c9892a4fc60a819f26a9c5
韩嘉浩 <1165411520@qq.com>
2024-10-11 18:01:10 +0800

    feat: 定义本地缓存 lru Priority

---
+++ cache/memory/lru/cache.go
@@ -0,0 +1,85 @@
+// Package lru
+/**
+* @Project : GenericGo
+* @File    : cache.go
+* @IDE     : GoLand
+* @Author  : Tvux
+* @Date    : 2024/10/11 14:33
+**/
+
+package lru
+
+import (
+	"context"
+	"github.com/HJH0924/GenericGo/cache"
+	"time"
+)
+
+var (
+	_ cache.Cache = (*Cache)(nil)
+)
+
+// Cache 是 cache.Cache 接口的实现，用于操作 LRU 缓存。
+// LRU - Least Recently Used
+type Cache struct {
+}
+
+func (Self *Cache) Set(ctx context.Context, key string, val any, expiration time.Duration) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (Self *Cache) SetNX(ctx context.Context, key string, val any, expiration time.Duration) (bool, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (Self *Cache) Get(ctx context.Context, key string) (any, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (Self *Cache) GetSet(ctx context.Context, key string, val any) (any, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (Self *Cache) Delete(ctx context.Context, keys ...string) (int64, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (Self *Cache) LPush(ctx context.Context, key string, vals ...any) (int64, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (Self *Cache) LPop(ctx context.Context, key string) (any, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (Self *Cache) SAdd(ctx context.Context, key string, members ...any) (int64, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (Self *Cache) SRem(ctx context.Context, key string, members ...any) (int64, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (Self *Cache) IncrBy(ctx context.Context, key string, value int64) (int64, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (Self *Cache) DecrBy(ctx context.Context, key string, decrement int64) (int64, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (Self *Cache) IncrByFloat(ctx context.Context, key string, value float64) (float64, error) {
+	//TODO implement me
+	panic("implement me")
+}
---
+++ cache/memory/priority/cache.go
@@ -0,0 +1,12 @@
+// Package priority
+/**
+* @Project : GenericGo
+* @File    : cache.go
+* @IDE     : GoLand
+* @Author  : Tvux
+* @Date    : 2024/10/11 14:37
+**/
+
+package priority
+
+// TODO 需要实现红黑树
